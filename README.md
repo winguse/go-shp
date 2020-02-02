88b0bcb0c2506a22f2c194968868b18f69c79b78
Yingyu Cheng <github@winguse.com>
2020-02-02 12:49:57 +0800

    update install script

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 Simple golang Secure HTTP Proxy implementation, support HTTP2 by default.
 
-To avoid proxy detection, it will act as reverse proxy unless providing correct authentication header. To allow Chrome extension to work well, it will request for authentication if the client is requesting for a special URL.
+To avoid proxy detection, it will act as reverse proxy unless providing correct authentication header. To allow Chrome extension to work well, it will request for authentication only if the client is requesting for a special URL.
 
 ## Install
+
+### Server
 
 0. You should have a domain and a server.
 1. Get a certificate from [Let’s Encrypt](https://letsencrypt.org), for example:
@@ -13,9 +15,18 @@ To avoid proxy detection, it will act as reverse proxy unless providing correct 
    $ sudo letsencrypt certonly -d YOUR_DOMAIN -m YOUR_EMAIL --agree-tos --standalone -n
    ```
 2. Download [`install.sh`](./install.sh)
-3. Make some changes, say the version you want to install, your domain name, your `user`s and `pass`s.
+3. Make some changes, say the version you want to install, your domain name, your `user`s and `password`s. If you are not using OAuth backend, remove that section, or please add the OAuth config (This project assumes you are using [Google OAuth](https://console.cloud.google.com/apis/credentials), but it should works with other platform).
 4. `$ chmod +x install.sh && ./install.sh`
-5. If you are using Chrome:
-  - Change [background.js](./chrome-minimum-client/background.js) accordingly
+
+### Client
+
+#### Basic usage
+
+If you are using OAuth backend, it will [render](./server/render.js) the client usage details for you after login.
+
+#### Chrome
+
+There is a plugin in the [`chrome-minimum-client`](./chrome-minimum-client/) directory.
+  - Make some changes according to your configs
   - Open `Menu` / `More Tools` / `Extensions`
   - Enable `Developer mode` on the top right and `Load unpacked` from [chrome-minimum-client](./chrome-minimum-client/).
--- install.sh
+++ install.sh
@@ -6,40 +6,38 @@ trigger407Token=SOME_SECRET_STRING
 clientID=SOME_CLIENT_ID
 clientSecret=SOME_CLIENT_SECRET
 redirectURL=SOME_REDIRECT_URL
+allowedEmail='^.+@allowed-domain.com$'
 
-curl https://github.com/winguse/go-shp/releases/download/$version/go-shp-linux-amd64-$version.gz -L | gzip -d - > /usr/bin/go-shp
+curl https://github.com/winguse/go-shp/releases/download/$version/go-shp-server-linux-amd64-$version.gz -L | gzip -d - > /usr/bin/go-shp
 chmod +x /usr/bin/go-shp
 
 mkdir -p /etc/go-shp/
 
-cat > /etc/go-shp/config.json <<-EOF
-{
-  "UpstreamAddr": "http://127.0.0.1:80",
-  "ListenAddr": ":443",
-  "CertFile": "/etc/letsencrypt/live/$domain/fullchain.pem",
-  "KeyFile": "/etc/letsencrypt/live/$domain/privkey.pem",
-  "Auth": {
-    "user": "pass"
-  },
-  "OAuthBackend": {
-    "OAuth": {
-      "ClientID": "$clientID",
-      "ClientSecret": "$clientSecret",
-      "Endpoint": {
-        "AuthURL": "https://accounts.google.com/o/oauth2/auth",
-        "TokenURL": "https://oauth2.googleapis.com/token",
-        "AuthStyle": 1
-      },
-      "RedirectURL": "$redirectURL",
-      "Scopes": ["https://www.googleapis.com/auth/userinfo.email"]
-    },
-    "TokenInfoAPI": "https://www.googleapis.com/oauth2/v1/tokeninfo"
-  },
-  "Trigger407Token": "$trigger407Token"
-}
+cat > /etc/go-shp/config.yaml <<-EOF
+upstream_addr: http://127.0.0.1:80
+listen_addr: ":443"
+cert_file: /etc/letsencrypt/live/$domain/fullchain.pem
+key_file: /etc/letsencrypt/live/$domain/privkey.pem
+auth: {}
+oauth_backend:
+  oauth:
+    client_id: '$clientID'
+    client_secret: '$clientSecret'
+    endpoint:
+      auth_url: https://accounts.google.com/o/oauth2/auth
+      token_url: https://oauth2.googleapis.com/token
+      auth_style: 1
+    redirect_url: $redirectURL
+    scopes:
+    - https://www.googleapis.com/auth/userinfo.email
+  token_info_api: https://www.googleapis.com/oauth2/v1/tokeninfo
+  render_js_src: https://wingu.se/go-shp/server/render.js
+  valid_email: '$allowedEmail'
+trigger_407_token: $trigger407Token
+
 EOF
 
-chmod 600 /etc/go-shp/config.json
+chmod 600 /etc/go-shp/config.yaml
 
 cat > /etc/systemd/system/go-shp.service <<-EOF
 [Unit]
